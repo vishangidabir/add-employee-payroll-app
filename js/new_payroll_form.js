6925874965b0e8af88ad0d3a1b344a9d6615bf81
vishangi <vishangid@gmail.com>
2022-08-10 15:25:20 +0530

    Added : Reset the employee payroll form

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -79,3 +79,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
         let value = document.getElementById(id).value;
         return value;
     }
+
+    const resetForm = () =>{
+        setValue('#name','');
+        unsetSelectedValues('[name=profile]');
+        unsetSelectedValues('[name=gender]');
+        unsetSelectedValues('[name=department]');
+        setValue('#salary','');
+        setValue('#notes','');
+        setValue('#day','');
+        setValue('#month','January');
+        setValue('#year','2020');
+    }
+
+    const unsetSelectedValues = (propertyValue) =>{
+        let allItems = document.querySelectorAll(propertyValue);
+        allItems.forEach(item => {
+            item.checked = false;
+        });
+    }
+
+    const setTextValue = (id, value) =>{
+        const element = document.querySelector(id);
+        element.textContent = value;
+    }
+
+    const setValue = (id, value) =>{
+        const element = document.querySelector(id);
+        element.value = value;
+    }
