5d5484f7b512cc5e3379351eee9d733611e27732
vishangi <vishangid@gmail.com>
2022-08-12 18:11:44 +0530

    Modified : Ability to view Employee payroll in tabular format from JS file

--- js/new_home.js
+++ js/new_home.js
@@ -6,21 +6,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Nane</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let innerHtml = `${headerHtml}`;{
-    innerHtml = `${innerHtml}
-            <tr>
-                <td><img class="profile" alt="" src="${employeePayrollList[index]._profilePic}"></td>
-                <td>${employeePayrollList[index]._name}</td>
-                <td>${employeePayrollList[index]._gender}</td>
-                <td>${getDeptHtml(employeePayrollList[index]._department)}</td>
-                <td>${employeePayrollList[index]._salary}</td>
-                <td>${stringifyDate(employeePayrollList[index]._startDate)}</td>
-                <td>
-                    <img id="${index}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-                    <img id="${index}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
-                </td>
-            </tr>
+    let innerHtml = `${headerHtml}
+   
+    <tr>
+        <td><img class="profile" src="../assets/assets/profile-images/Ellipse -2.png" alt=""></td>
+        <td>Narayan Mahadevan</td>
+        <td>Male</td>
+        <td><div class="dept-label">HR</div><div class="dept-label">Finance</div></td>
+        <td>3000000</td>
+        <td>1 Nov 2020</td>
+        <td>
+            <img id="1" onclick="remove(this)" src="../assets/assets/icons/delete-black-18dp.svg" alt="delete">
+            <img id="1" onclick="update(this)" src="../assets/assets/icons/create-black-18dp.svg" alt="edit">
+        </td>
+    </tr>
         `;
-    }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
